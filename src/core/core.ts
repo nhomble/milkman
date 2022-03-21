1bb9a70d16b21adc8651028bf225c5fe1ee70b85
nhomble <nhomble@terpmail.umd.edu>
2022-03-20 21:36:17 -0400

    asyncs have order now

--- src/core/core.ts
+++ src/core/core.ts
@@ -3,6 +3,7 @@ import { load } from "js-yaml";
 import * as fs from "fs";
 import * as path from "path";
 import axios from "axios";
+import chalk from "chalk";
 
 /**
  * find names of all resources
@@ -10,12 +11,15 @@ import axios from "axios";
  * @returns array of names
  */
 export const discoverNames = function (root: string): string[] {
-  const paths = discoverMilk(root).map((res) => res.metadata.name);
+  const paths = createSchedule(discoverMilk(root)).map(
+    (res) => `${res.metadata.name}`
+  );
   return paths;
 };
 
 export type MilkMetadata = {
   name: string;
+  path: string;
   labels: Map<String, String>;
 };
 
@@ -27,7 +31,7 @@ export type MilkResource = {
 };
 
 export type ScriptSpec = {
-  depends: string[];
+  dependsOn: string[];
   script: string;
 };
 
@@ -36,7 +40,7 @@ export type RequestSpec = {
   host: string;
   route: string;
   method: "GET" | "POST";
-  depends: string[];
+  dependsOn: string[];
 };
 
 /**
@@ -51,6 +55,7 @@ export const discoverResources = function (root: string): string[] {
 };
 
 export const discoverMilk = function (root: string): MilkResource[] {
+  // TODO check for dupe names
   return discoverResources(root).map((path) => {
     return parseYamlResource(path);
   });
@@ -70,6 +75,7 @@ const parseYamlResource = function (path: string): MilkResource {
     throw new Error(`${path} does not have kind defined`);
   }
 
+  resource.metadata.path = path;
   return resource;
 };
 
@@ -85,31 +91,108 @@ const executeWithContext = function (
   resources: MilkResource[],
   context: Map<string, any>
 ): Promise<any> {
-  return Promise.all(
-    createSchedule(resources).map((resource) => {
+  const asyncs: CallableFunction[] = createSchedule(resources).map(
+    (resource) => {
       switch (resource.kind) {
         case "Request":
-          return executeRequest(resource, context);
+          return async () => {
+            return executeRequest(resource, context);
+          };
         case "Script":
-          return executeScript(resource, context);
+          return async () => {
+            executeScript(resource, context);
+          };
       }
-    })
+    }
+  );
+  return asyncs.reduce(
+    (promise, func) =>
+      promise.then(() =>
+        func()
+      ),
+    Promise.resolve()
   );
 };
 
 export const createSchedule = function (
   resources: MilkResource[]
 ): MilkResource[] {
-  return resources;
+  const name_to_resource = new Map<string, MilkResource>();
+
+  const all_names = resources.map((resource) => {
+    const n = resource.metadata.name;
+    name_to_resource.set(n, resource);
+    return n;
+  });
+  const graph = new Map<string, string[]>();
+  const in_degrees = new Map<string, number>();
+  // init the data structures
+  all_names.forEach((name) => {
+    graph.set(name, []);
+    in_degrees.set(name, 0);
+  });
+
+  // dependency graph
+  resources.forEach((resource) => {
+    const dependsOn = resource.spec.dependsOn || [];
+    const name = resource.metadata.name;
+    dependsOn.forEach((dependency) => {
+      // update edges
+      const all_deps = graph.get(dependency) || [];
+      all_deps?.push(name);
+      graph.set(dependency, all_deps);
+
+      // edges are directional, keep track of indegrees to a node
+      // for every dependency from `dependsOn` the current resource
+      // `name` must increment the indegree
+      in_degrees.set(name, (in_degrees.get(name) || 0) + 1);
+    });
+  });
+
+  // bfs respecting indegree
+
+  const ret: string[] = [];
+  const working: string[] = [];
+  in_degrees.forEach((value, key) => {
+    if (value == 0) {
+      working.push(key);
+    }
+  });
+  while (working.length > 0) {
+    const curr = working.shift()!;
+    ret.push(curr);
+
+    const children = graph.get(curr)!;
+    children.forEach((child) => {
+      const i = in_degrees.get(child)!;
+      in_degrees.set(child, i - 1);
+
+      if (i - 1 == 0 && ret.indexOf(child) == -1) {
+        working.push(child);
+      }
+    });
+  }
+  return ret.map((n) => name_to_resource.get(n)!);
 };
 
 export const executeScript = function (
   resource: MilkResource,
   context: Map<string, any>
-): Map<string, any> {
+): Promise<any> {
   const spec = resource.spec as ScriptSpec;
-  Function("context", spec.script)(context);
-  return context;
+  const newConsole = {
+    log: function (s: object) {
+      console.log(`${chalk.blueBright(resource.metadata.name + ">")}`, s);
+    },
+    warn: function (s: object) {
+      console.log(`${chalk.yellowBright(resource.metadata.name + ">")}`, s);
+    },
+    error: function (s: object) {
+      console.log(`${chalk.redBright(resource.metadata.name + ">")}`, s);
+    },
+  };
+  Function("context", "console", spec.script)(context, newConsole);
+  return Promise.resolve(context);
 };
 
 export const executeRequest = async function (
