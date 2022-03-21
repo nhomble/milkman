c7b8aa7304c4191cdf0ccd46e35569ed7d2a909f
nhomble <nhomble@terpmail.umd.edu>
2022-03-20 21:56:29 -0400

    add scheduler

--- src/core/core.ts
+++ src/core/core.ts
@@ -4,6 +4,7 @@ import * as fs from "fs";
 import * as path from "path";
 import axios from "axios";
 import chalk from "chalk";
+import { createSchedule } from "./schedule";
 
 /**
  * find names of all resources
@@ -112,67 +113,6 @@ const executeWithContext = function (
       ),
     Promise.resolve()
   );
-};
-
-export const createSchedule = function (
-  resources: MilkResource[]
-): MilkResource[] {
-  const name_to_resource = new Map<string, MilkResource>();
-
-  const all_names = resources.map((resource) => {
-    const n = resource.metadata.name;
-    name_to_resource.set(n, resource);
-    return n;
-  });
-  const graph = new Map<string, string[]>();
-  const in_degrees = new Map<string, number>();
-  // init the data structures
-  all_names.forEach((name) => {
-    graph.set(name, []);
-    in_degrees.set(name, 0);
-  });
-
-  // dependency graph
-  resources.forEach((resource) => {
-    const dependsOn = resource.spec.dependsOn || [];
-    const name = resource.metadata.name;
-    dependsOn.forEach((dependency) => {
-      // update edges
-      const all_deps = graph.get(dependency) || [];
-      all_deps?.push(name);
-      graph.set(dependency, all_deps);
-
-      // edges are directional, keep track of indegrees to a node
-      // for every dependency from `dependsOn` the current resource
-      // `name` must increment the indegree
-      in_degrees.set(name, (in_degrees.get(name) || 0) + 1);
-    });
-  });
-
-  // bfs respecting indegree
-
-  const ret: string[] = [];
-  const working: string[] = [];
-  in_degrees.forEach((value, key) => {
-    if (value == 0) {
-      working.push(key);
-    }
-  });
-  while (working.length > 0) {
-    const curr = working.shift()!;
-    ret.push(curr);
-
-    const children = graph.get(curr)!;
-    children.forEach((child) => {
-      const i = in_degrees.get(child)!;
-      in_degrees.set(child, i - 1);
-
-      if (i - 1 == 0 && ret.indexOf(child) == -1) {
-        working.push(child);
-      }
-    });
-  }
-  return ret.map((n) => name_to_resource.get(n)!);
 };
 
 export const executeScript = function (
